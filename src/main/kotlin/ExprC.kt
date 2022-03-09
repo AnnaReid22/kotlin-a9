4fdfa51bf89b6538f463f01d739c7a03c53d646c
Chris-Middleton <middleton.chris22@gmail.com>
2022-03-08 16:26:09 -0800

    Parsing, and toString for ExprC's

--- src/main/kotlin/ExprC.kt
+++ src/main/kotlin/ExprC.kt
@@ -1,8 +1,41 @@
 abstract class ExprC
 
-class NumC(val num: Int) : ExprC()
-class IdC(val id: String) : ExprC()
-class AppC(val fn: ExprC, val args: ArrayList<ExprC>) : ExprC()
-class LamC(val params: ArrayList<IdC>, val body: ExprC) : ExprC()
-class StringC(val s: String) : ExprC()
-class IfC(val self: ExprC, val then: ExprC, val otherwise: ExprC) : ExprC()
+class NumC(val num: Int) : ExprC(){
+    override fun toString(): String { return "(NumC $num)" }
+}
+class IdC(val id: String) : ExprC(){
+    override fun toString(): String { return "(IdC $id)" }
+}
+class StringC(val s: String) : ExprC(){
+    override fun toString(): String { return "(StringC \"$s\")" }
+}
+class AppC(val fn: ExprC, val args: ArrayList<ExprC>) : ExprC(){
+    override fun toString(): String{
+        var sb = StringBuilder("(AppC ${fn.toString()} (list");
+        for (arg in args){
+            sb.append(" ");
+            sb.append(arg.toString());
+        }
+        sb.append("))")
+        return sb.toString();
+    }
+}
+class LamC(val params: ArrayList<IdC>, val body: ExprC) : ExprC(){
+    override fun toString(): String{
+        var sb = StringBuilder("(LamC (");
+        sb.append("list");
+        for (id in params){
+            sb.append(" ");
+            sb.append(id.toString());
+        }
+        sb.append(") ");
+        sb.append(body.toString());
+        sb.append(")");
+        return sb.toString();
+    }
+}
+class IfC(val self: ExprC, val then: ExprC, val otherwise: ExprC) : ExprC(){
+    override fun toString(): String{
+        return "(IfC ${self.toString()} ${then.toString()} ${otherwise.toString()})";
+    }
+}
